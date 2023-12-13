471072e79602c5b01dd03c7947a0235fc5e0187a
Sümeyye Karataş <121102284+karatasumeyye@users.noreply.github.com>
2023-12-13 13:25:06 +0300

    Add files via upload

---
+++ Face_Recognition/main.py
@@ -0,0 +1,32 @@
+import cv2
+from simple_facerec import SimpleFacerec
+
+#Encode faces from a folder
+sfr=SimpleFacerec()
+sfr.load_encoding_images("images/")  #all picture
+
+
+#Load camera
+cap= cv2.VideoCapture(0)  # zero mean: first camera
+
+
+while True:
+    ret,frame=cap.read()
+
+    #detect faces
+    face_locations, face_names=sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations,face_names):
+        y1,x1,y2,x2 = face_loc[0],face_loc[1],face_loc[2],face_loc[3]     #[288 564 480 356]  location
+
+        cv2.putText(frame,name,(x1,y1-10), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),2)
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,200),4)
+
+    cv2.imshow("Frame",frame)
+
+    key=cv2.waitKey(1)  # 1 millisecond and go to the next frame
+    if key==27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
